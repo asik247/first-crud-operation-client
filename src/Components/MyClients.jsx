77e8e33f76fca8dd605636658c095b59a315b99d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-17 00:45:47 +0600

    user data send server site

--- src/Components/MyClients.jsx
+++ src/Components/MyClients.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const MyClients = () => {
+    // HandlerSubmit 
+    const handlerSubmit = (e)=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        console.log(name,email);
+    }
     return (
         <div>
              <div className="hero-content ">
@@ -10,12 +17,14 @@ const MyClients = () => {
                
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={handlerSubmit}>  
                                 <fieldset className="fieldset">
                                     <label className="label">Name</label>
-                                    <input type="text" className="input" placeholder="Name" />
+                                    <input type="text" name='name' className="input" placeholder="Name" />
+
                                     <label className="label">Email</label>
-                                    <input type="email" className="input" placeholder="Email" />
+                                    <input type="email" name='email' className="input" placeholder="Email" />
+
                                     <button className="btn btn-neutral mt-4">Add User</button>
                                 </fieldset>
                             </form>
