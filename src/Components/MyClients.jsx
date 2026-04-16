a72a210cc2dd99b4ddb3d0ef9f4f983c06c3024c
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-17 00:39:45 +0600

    post method usign in data db set

--- src/Components/MyClients.jsx
+++ src/Components/MyClients.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const MyClients = () => {
     return (
         <div>
-            <h2>Client hre</h2>
+             <div className="hero-content ">
+                   
+            <div className="hero bg-base-200 min-h-screen">
+
+               
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <form>
+                                <fieldset className="fieldset">
+                                    <label className="label">Name</label>
+                                    <input type="text" className="input" placeholder="Name" />
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <button className="btn btn-neutral mt-4">Add User</button>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
